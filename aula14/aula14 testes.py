14550b2333a5e71f100f3950e47bc5ef893cb1c8
BernardoBastos-2014 <bernardo.bastos2014@outlook.com>
2026-08-31 13:59:10 -0300

    Apagou tudo

--- aula14/aula14 testes.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from aula14.aula14 import sob
-class NomeTest(unittest.TestCase):
-    def test_sobrenome(self):
-        resultado = sob("jão", "madera", "sos")
-        self.assertEqual(resultado, "jão madera sos")
-if __name__ == '__main__':
-    unittest.main(argv=[''], exit=False)
--- aula14/matematica.py
+++
@@ -1,5 +0,0 @@
-class cauculadora():
-    def soma(self,a,b):
-        return a + b 
-    def sub(self,a,b):
-        return a - b 
